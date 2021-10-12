84624f3e18212ad336ec65fc1895a7e118a926bd
Oksana Palamarchuk <8444186@gmail.com>
2021-10-12 18:06:29 +0200

    Update favorite status method

--- controllers/updateStatusContact.js
+++ controllers/updateStatusContact.js
@@ -3,7 +3,8 @@ const { Contact } = require('../models/contacts');
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+  const { favorite } = req.body;
+  const result = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} not found`);
   }
