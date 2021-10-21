e326e463a8b66c7f23338f82ea1762c3b4dd3717
Oksana Palamarchuk <8444186@gmail.com>
2021-10-21 17:21:28 +0200

    Create users controllers

---
+++ controllers/users/updateSubscription.js
@@ -0,0 +1,20 @@
+const { NotFound } = require('http-errors');
+const { User } = require('../../models/users');
+
+const updateSubscription = async (req, res) => {
+  const { _id } = req.user;
+  const { subscription } = req.body;
+  const result = await User.findByIdAndUpdate(_id, { subscription }, { new: true });
+  if (!result) {
+    throw new NotFound(`User with id=${_id} not found`);
+  }
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result
+    }
+  });
+};
+
+module.exports = updateSubscription;
