ffae596d6f0688ad7e29c0732fb827982b7b054d
Oksana Palamarchuk <8444186@gmail.com>
2021-10-31 22:59:50 +0100

    Add default avatar during signup

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,6 @@
 const { Conflict } = require('http-errors');
 const { User } = require('../../models/users');
+const gravatar = require('gravatar');
 
 const signup = async(req, res) => {
   const { email, password } = req.body;
@@ -7,7 +8,8 @@ const signup = async(req, res) => {
   if (user) {
     throw new Conflict('Email in use');
   }
-  const newUser = new User({ email });
+  const avatarURL = gravatar.url(email);
+  const newUser = new User({ email, avatarURL });
   newUser.setPassword(password);
   await newUser.save();
 
