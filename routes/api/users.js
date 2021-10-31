2e3fcd9218e56e9da2626c89a3d99865edfab907
Oksana Palamarchuk <8444186@gmail.com>
2021-10-31 23:00:57 +0100

    Create router for updating avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { users } = require('../../controllers');
-const { controllerWrapper, validation, authenticate } = require('../../middlewares');
+const { controllerWrapper, validation, authenticate, upload } = require('../../middlewares');
 const { subscriptionSchema } = require('../../models/users/user');
 
 router.get('/current', authenticate, controllerWrapper(users.getCurrentUser));
 
 router.patch('/', authenticate, validation(subscriptionSchema), controllerWrapper(users.updateSubscription));
+
+router.patch('/avatars', authenticate, upload.single('avatar'), controllerWrapper(users.updateAvatar));
 
 module.exports = router;
